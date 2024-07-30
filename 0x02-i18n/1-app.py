72f9b1c3c860f7c9c3885a15640de1cf95251788
dunsinadesina <adesinajesudunsin@gmail.com>
2024-07-30 05:00:28 -0700

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,31 +1,36 @@
-#!/usr/bin/python3
-"""basic flask app"""
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
 from flask import Flask
 from flask import render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    """configuration class"""
+    """
+    Application configuration class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-    #instantitate application object
-    app = Flask(__name__)
-    app.config.from_object(Config)
+# Instantiate the application object
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
+babel = Babel(app)
 
 
-    #wrap application with babel
-    babel = Babel(app)
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
 
 
-    @app.route('/', strict_slashes=False)
-    def index():
-        """renders basic html template"""
-        return render_template('1-index.html')
-
-
-    if __name__ == '__main__':
-        app.run()
+if __name__ == '__main__':
+    app.run()
